90f53cc6f226aad947430d8516baf1ac11c84c65
knopfus <gavin@kidsmail.ch>
2023-04-23 15:54:41 +0200

    2 neue Räume

--- PlanetCharc.js
+++ PlanetCharc.js
@@ -16,10 +16,88 @@ window.Orte = {
             gehe_zu: { Ziel_Ort_Name: "Silberne_Lichtung", Skyly_10: { links: 420, oben: 255, kommt_von: { links: 110, oben: 410 } }, Skyly_12: { links: 310, oben: 445, kommt_von: { links: 0, oben: 300 } } }
         }
 
+    },
+
+    Lavawelt: {
+
+        mögliche_Aktionen: { 
+            gehe_zu: { Ziel_Ort_Name: "Dschungel_1", Skyly_10: { links: 420, oben: 255, kommt_von: { links: 110, oben: 410 } }, Skyly_12: { links: 310, oben: 445, kommt_von: { links: 0, oben: 300 } } }
+        }
+
+    },
+
+    Grosse_Wiese: {
+
+        mögliche_Aktionen: { 
+            gehe_zu: { Ziel_Ort_Name: "Dschungel_1", Skyly_10: { links: 420, oben: 255, kommt_von: { links: 110, oben: 410 } }, Skyly_12: { links: 310, oben: 445, kommt_von: { links: 0, oben: 300 } } }
+        }
+
     }
 };
 
 window.Gegenstände = {
+
+
+    /* Beispiele:
+
+    "Computer": {
+
+        wo: { Ort_Name: "Zimmer_Gegenwart", links: 500, oben: 350, breit: 100, hoch: 75 },
+
+        mögliche_Aktionen: {
+            nehmen: { wo: "im_Ort" },
+            lernen: { wo: "im_Besitz", zusätzlicher_IQ: 3, gelernt: false }
+        }
+
+    },
+
+    "Zeitmaschine_Höhle_Vergangenheit": {
+
+        wo: { Ort_Name: "Höhle_Vergangenheit", links: 619, oben: -9, breit: 100, hoch: 170, gedreht: 263 },
+
+        mögliche_Aktionen: {
+            gehe_zu: {
+                wo: "im_Ort",
+                Ziel_Ort_Name: "Zimmer_Gegenwart",
+                Luan_10: { links: 520, oben: 72, kommt_von: { links: 550, oben: 71 } },
+                Luan_12: { links: 400, oben: 72, kommt_von: { links: 300, oben: 72 } }
+            }
+        }
+
+    }
+    */
+    
+    "Portal_von_Dschungel_1_zu_Lavawelt": {    
+       
+        wo: { Ort_Name: "Dschungel_1", links: 1, oben: 1, breit: 100, hoch: 170 },
+
+        mögliche_Aktionen: {
+            
+            gehe_zu: {
+                wo: "im_Ort",
+                Ziel_Ort_Name: "Lavawelt",
+                Skyly_10: { links: 420, oben: 255, kommt_von: { links: 110, oben: 410 } }, 
+                Skyly_12: { links: 310, oben: 445, kommt_von: { links: 0, oben: 300 } }
+            }
+        
+        }
+    },
+
+    "Portal_von_Dschungel_1_zu_Grosse_Wiese": {    
+       
+        wo: { Ort_Name: "Dschungel_1", links: 100, oben: 1, breit: 100, hoch: 170 },
+
+        mögliche_Aktionen: {
+            
+            gehe_zu: {
+                wo: "im_Ort",
+                Ziel_Ort_Name: "Grosse_Wiese",
+                Skyly_10: { links: 420, oben: 255, kommt_von: { links: 110, oben: 410 } }, 
+                Skyly_12: { links: 310, oben: 445, kommt_von: { links: 0, oben: 300 } }
+            }
+        
+        }
+    }
 
 };
 
