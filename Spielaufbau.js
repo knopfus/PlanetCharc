89338539dd4d8eddc06d7c0d97ec13fe4940be2c
knopfus <gavin@kidsmail.ch>
2024-01-04 10:49:30 +0100

    Reich_des_Giganten in Daniels Code

--- Spielaufbau.js
+++ Spielaufbau.js
@@ -3,51 +3,55 @@ var Spielaufbau = {
 
     Orte: {
 
-        Silberne_Lichtung:  { Portale: {
+        Silberne_Lichtung: { Portale: {
             Nord: { zu: "Dschungel_1",          links: 50, oben: 0, breit: 1340, hoch: 50 },
             Ost:  { zu: "Dschungel_2",          links: 1390, oben: 50, breit: 50, hoch: 611 }
         } },
-        Dschungel_1:        { Portale: {
+        Dschungel_1: { Portale: {
             Nord: { zu: "Berge_der_Angst",      links: 50, oben: 0, breit: 1340, hoch: 50 },
             Süd:  { zu: "Silberne_Lichtung",    links: 50, oben: 661, breit: 1340, hoch: 50 },
             West: { zu: "Grosse_Wiese",         links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Berge_der_Angst:    { Portale: {
+        Berge_der_Angst: { Portale: {
             Ost:  { zu: "Lavawelt",             links: 1390, oben: 50, breit: 50, hoch: 611 },
             Süd:  { zu: "Grosse_Wiese",         links: 50, oben: 661, breit: 1340, hoch: 50 },
             West: { zu: "Gipfel",               links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Gipfel:             { Portale: {
+        Gipfel: { Portale: {
             Ost:  { zu: "Berge_der_Angst",      links: 1390, oben: 50, breit: 50, hoch: 611 }
         } },
-        Grosse_Wiese:       { Portale: {
+        Grosse_Wiese: { Portale: {
             Nord: { zu: "Berge_der_Angst",      links: 50, oben: 0, breit: 1340, hoch: 50 },
             Ost:  { zu: "Dschungel_1",          links: 1390, oben: 50, breit: 50, hoch: 611 },
             West: { zu: "Ewiges_Eis",           links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Ewiges_Eis:         { Portale: {
+        Ewiges_Eis: { Portale: {
             Ost:  { zu: "Grosse_Wiese",         links: 1390, oben: 50, breit: 50, hoch: 611 },
             West: { zu: "Höhle_Yeti",           links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Höhle_Yeti:         { Portale: {
+        Höhle_Yeti: { Portale: {
             Ost:  { zu: "Ewiges_Eis",           links: 1390, oben: 50, breit: 50, hoch: 611 }
         } },
 
-        Dschungel_2:        { Portale: {
+        Dschungel_2: { Portale: {
             Ost:  { zu: "Fluss",                links: 1390, oben: 50, breit: 50, hoch: 611 },
             West: { zu: "Silberne_Lichtung",    links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Fluss:              { Portale: {
+        Fluss: { Portale: {
             Nord: { zu: "Quelle_des_Lichts",    links: 50, oben: 0, breit: 1340, hoch: 50 },
             West: { zu: "Dschungel_2",          links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
-        Quelle_des_Lichts:  { Portale: {
+        Quelle_des_Lichts: { Portale: {
             Nord: { zu: "Lavawelt",             links: 50, oben: 0, breit: 1340, hoch: 50 },
             Süd:  { zu: "Fluss",                links: 50, oben: 661, breit: 1340, hoch: 50 }
         } },
-        Lavawelt:          { Portale: {
+        Lavawelt: { Portale: {
+            Ost:  { zu: "Reich_des_Giganten",   links: 1390, oben: 50, breit: 50, hoch: 611 },
             Süd:  { zu: "Quelle_des_Lichts",    links: 50, oben: 661, breit: 1340, hoch: 50 },
             West: { zu: "Berge_der_Angst",      links: 0, oben: 50, breit: 50, hoch: 611 }
+        } },
+        Reich_des_Giganten: { Portale: {
+            West: { zu: "Lavawelt",             links: 0, oben: 50, breit: 50, hoch: 611 }
         } },
 
         Unerreichbarer_Ort: { }
